0573328093c194bdff01a12cf10837aada2245e6
daneal2u <daneal2u@gmail.com>
2016-05-27 17:29:01 +0400

    DU-ify things!

--- du.mk
+++ du.mk
@@ -17,15 +17,15 @@
 # Release name
 PRODUCT_RELEASE_NAME := GT-N7000
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7000/full_n7000.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7000
-PRODUCT_NAME := cm_n7000
+PRODUCT_NAME := du_n7000
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-N7000
 
